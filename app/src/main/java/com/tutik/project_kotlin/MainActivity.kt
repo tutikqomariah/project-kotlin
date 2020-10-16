9375a4f603945c51c3f9c3773bfdd9c754659c0d
tutikqomariah <71108601+tutikqomariah@users.noreply.github.com>
2020-10-16 09:08:44 +0700

    Update Project-Kotlin

--- app/src/main/java/com/tutik/project_kotlin/MainActivity.kt
+++ app/src/main/java/com/tutik/project_kotlin/MainActivity.kt
@@ -2,10 +2,64 @@ package com.tutik.project_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setDataSpinnerGender()
+        btnSave.setOnClickListener { validasiInput() }
+    }
+    private fun goToProfilActivity() {
+        val bundle = Bundle()
+        bundle.putString("nama", namaInput)
+        bundle.putString("gender", genderInput)
+        bundle.putString("email", emailInput)
+        bundle.putString("telp", telpInput)
+        bundle.putString("alamat", alamatInput)
+        intent.putExtras(bundle)
+
+        val intent = Intent(this, ProfilActivity::class.java)
+        startActivity(intent)
+    }
+
+    private var namaInput : String = ""
+    private var emailInput : String = ""
+    private var telpInput : String = ""
+    private var alamatInput : String = ""
+    private var genderInput : String = ""
+    private fun validasiInput(){
+        namaInput = edtName.text.toString()
+        emailInput = edtEmail.text.toString()
+        telpInput = edtTelp.text.toString()
+        alamatInput = edtAddress.text.toString()
+        genderInput = spinnerGender.selectedItem.toString()
+        when{
+            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
+            genderInput.equals("Pilih Jenis Kelamin", ignoreCase = true) ->
+                tampilToast("Jenis Kelamin harus dipilih")
+
+            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
+            else -> {
+                tampilToast("Navigasi ke halaman profil")
+                goToProfilActivity()
+            }
+        }
+
+    }
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun setDataSpinnerGender(){
+        val adapter = ArrayAdapter.createFromResource(this,
+            R.array.jenis_kelamin, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerGender.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/tutik/project_kotlin/ProfilActivity.kt
@@ -0,0 +1,27 @@
+package com.tutik.project_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profil.*
+
+class ProfilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+        ambilData()
+    }
+    private fun ambilData(){
+        val bundle = intent.extras
+        val nama = bundle?.getString("nama")
+        val gender = bundle?.getString("gender")
+        val email = bundle?.getString("email")
+        val telp = bundle?.getString("telp")
+        val alamat = bundle?.getString("alamat")
+
+        txtName.text = nama
+        txtGender.text = gender
+        txtEmail.text = email
+        txtTelp.text = telp
+        txtAddress.text = alamat
+    }
+}
