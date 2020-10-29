816cacd176ecb60dfd72932f6bc91bb8e679f084
tutikqomariah <71108601+tutikqomariah@users.noreply.github.com>
2020-10-29 21:58:32 +0700

    Perbaikan Edit Profil

--- app/src/main/java/com/tutik/project_kotlin/EditProfilActivity.kt
+++ app/src/main/java/com/tutik/project_kotlin/EditProfilActivity.kt
@@ -7,34 +7,33 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_profil.*
 
 class EditProfilActivity : AppCompatActivity() {
+    private var namaEdit : String = ""
+    private var emailEdit : String = ""
+    private var telpEdit : String = ""
+    private var alamatEdit : String = ""
+    private var genderEdit : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profil)
-    //menerima data yang dikirimkan dari ProfilActivity.kt
         val intentData = intent.extras
         val namaUser = intentData?.getString("nama")
-        val genderUser = intentData?.getString("gender")
         val emailUser = intentData?.getString("email")
         val telpUser = intentData?.getString("telp")
         val alamatUser = intentData?.getString("alamat")
         edtProfilName.setText(namaUser)
-        edtProfilGender.setText(genderUser)
         edtProfilEmail.setText(emailUser)
         edtProfilTelp.setText(telpUser)
         edtProfilAlamat.setText(alamatUser)
-    //memberikan action click ke tombol Simpan
         btnEditSave.setOnClickListener { saveData() }
-    }
-    //mengirimkan kembali ke ProfilActivity.kt
-    private fun saveData(){
 
-        val namaEdit = edtProfilName.text.toString()
-        val genderEdit = edtProfilGender.text.toString()
-        val emailEdit = edtProfilEmail.text.toString()
-        val telpEdit = edtProfilTelp.text.toString()
-        val alamatEdit = edtProfilAlamat.text.toString()
-        if (!namaEdit.isEmpty() || !genderEdit.isEmpty() || !emailEdit.isEmpty() || !telpEdit.isEmpty() || !alamatEdit.isEmpty()) {
-    //jika editText namaEdit tidak kosong, maka kirimkan value nya ke ProfilActivity, dan beri tanda RESULT_OK
+    }
+    private fun saveData(){
+        namaEdit = edtProfilName.text.toString()
+        genderEdit = spinnerProfilGender.selectedItem.toString()
+        emailEdit = edtProfilEmail.text.toString()
+        telpEdit = edtProfilTelp.text.toString()
+        alamatEdit = edtProfilAlamat.text.toString()
+        if (!namaEdit.isEmpty() ) {
             val result = Intent()
             result.putExtra("nama", namaEdit)
             result.putExtra("gender", genderEdit)
@@ -43,9 +42,9 @@ class EditProfilActivity : AppCompatActivity() {
             result.putExtra("alamat", alamatEdit)
             setResult(Activity.RESULT_OK, result)
         } else {
-            //jika editText namaEdit kosong, maka kirimkan tanda RESULT_CANCELED
             setResult(Activity.RESULT_CANCELED)
         }
         finish()
     }
+
 }
